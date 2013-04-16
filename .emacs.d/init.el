b22fe2043847e8ffd9e81c10c2107bbde2b50508
Michel Metzger <michel.metzger@nuance.com>
2013-04-16 17:36:38 -0400

    Set font size to 12

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,3 +33,9 @@
           '(lambda ()
              (require 'groovy-electric)
              (groovy-electric-mode)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 120)))))
